6fe5445db31bdf0bd15e3b12089c9925beb8fe10
franck-g44 <franck44250@gmail.com>
2019-10-22 16:27:28 +0200

    page création article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,10 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Author;
 use App\Repository\ArticleRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class ArticleController extends AbstractController
 {
@@ -34,9 +39,9 @@ class ArticleController extends AbstractController
     }
 
     /**
-     * @Route("/new")
+     * @Route("/new", methods={"GET","POST"})
      */
-    public function new(){
+    public function new(Request $request){
         $article = new Article();
 
         $form = $this->createFormBuilder($article)
@@ -44,8 +49,28 @@ class ArticleController extends AbstractController
                 'label' => 'Titre',
             ])
             ->add('content')
-            ->add('publishedAt')
+            ->add('publishedAt', DateType::class, [
+                'widget' => 'single_text',
+                'required' => false,
+            ])
+            ->add('writtenBy', EntityType::class, [
+                'class' => Author::class,
+                'choice_label' => 'name'
+            ])
             ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('app_article_show', [
+                'id' => $article->getId(),
+            ]);
+
+        }
 
         return $this->render('article/new.html.twig',[
             'form' => $form->createView(),
